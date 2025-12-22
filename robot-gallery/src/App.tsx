839752654c881187ff00d7de5fa8e527ef0085b5
13756613348 <1456725324@qq.com>
2025-12-22 10:46:32 +0800

    更新

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -8,31 +8,83 @@ import ShoppingCart from "./components/ShoppingCart";
 // const html = "<img onerror='alert(\"Hacked!\")' src='invalid-image' />";
 // const jsHacked = "javascript:alert('Hacked!');";
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <div>Robot</div>
-        {/* <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1> */}
+interface Props {}
+interface State {
+  robotGallery: any[]; // 使用any类型原因，使用 any 类型,typescript类型还有意义吗 数据来源于网络请求，返回的数据类型不受控制，前端强行定义API数据类型，违反前后端分离的原则，不能为了使用Type而放弃JavaScript的灵活性
+}
+
+// function App() {
+//   return (
+//     <div className={styles.app}>
+//       <div className={styles.appHeader}>
+//         <img src={logo} className={styles.appLogo} alt="logo" />
+//         <div>Robot</div>
+//         {/* <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1> */}
+//       </div>
+//       <ShoppingCart />
+//       <div className={styles.robotList}>
+//         {robots.map((r) => (
+//           <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
+//         ))}
+//       </div>
+//     </div>
+//     // <>
+//     //   {/* <div>{html}</div>
+//     //   <a href={jsHacked}>My websit</a> */}
+//     //   <ul>
+//     //     {robots.map((r) => (
+//     //       <Robot id={r.id} name={r.name} email={r.email} />
+//     //     ))}
+//     //   </ul>
+//     // </>
+//   );
+// }
+
+// export default App;
+class App extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robotGallery: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          robotGallery: data,
+        });
+      });
+  }
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <div>Robot</div>
+          {/* <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1> */}
+        </div>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map((r) => (
+            <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
+          ))}
+        </div>
       </div>
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {robots.map((r) => (
-          <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
-        ))}
-      </div>
-    </div>
-    // <>
-    //   {/* <div>{html}</div>
-    //   <a href={jsHacked}>My websit</a> */}
-    //   <ul>
-    //     {robots.map((r) => (
-    //       <Robot id={r.id} name={r.name} email={r.email} />
-    //     ))}
-    //   </ul>
-    // </>
-  );
+      // <>
+      //   {/* <div>{html}</div>
+      //   <a href={jsHacked}>My websit</a> */}
+      //   <ul>
+      //     {robots.map((r) => (
+      //       <Robot id={r.id} name={r.name} email={r.email} />
+      //     ))}
+      //   </ul>
+      // </>
+    );
+  }
 }
 
 export default App;
--- robot-gallery/src/components/ShoppingCart.tsx
+++ robot-gallery/src/components/ShoppingCart.tsx
@@ -1,29 +1,43 @@
 import React from "react";
 import styles from "./ShoppingCart.module.css";
+import { FiShoppingCart } from "react-icons/fi";
 
 interface Props {}
 interface State {
   isOpen: boolean;
 }
+const CartIcon = FiShoppingCart as React.ComponentType<any>;
 class ShoppingCart extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
       isOpen: false,
     };
+    // this.handleClick = this.handleClick.bind(this); // bind只绑定this和预设参数，返回新函数，不执行
   }
+  // handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+  //   this.setState({
+  //     isOpen: !this.state.isOpen,
+  //   });
+  // }
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log("e.target ", e.target); // target 描述的是事件发生的元素（原始元素）
+    console.log("e.currentTarget ", e.currentTarget); // 而currentTarget描述的是事件处理绑定的元素（当前元素）
+    // this.setState({
+    //   isOpen: !this.state.isOpen,
+    // });
+    if ((e.target as HTMLElement).nodeName === "SPAN") {
+      this.setState({
+        isOpen: !this.state.isOpen,
+      });
+    }
+  };
   render() {
     return (
       <div className={styles.cartContainer}>
-        <button
-          className={styles.button}
-          onClick={() => {
-            this.setState({
-              isOpen: !this.state.isOpen,
-            });
-          }}
-        >
-          xxx2（件）
+        <button className={styles.button} onClick={this.handleClick}>
+          <CartIcon />
+          <span>xxx2（件）</span>
         </button>
         <div
           className={styles.cartDropDown}
